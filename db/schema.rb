0585e41b9421c7e1d06afd87ea3b3e03a307f68e
Hector Gonzalez <hectorgz25@icloud.com>
2022-10-08 14:39:04 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_171813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 ActiveRecord::Schema[7.0].define(version: 2022_10_08_171439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_171439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reservations_on_user_id"
+
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
